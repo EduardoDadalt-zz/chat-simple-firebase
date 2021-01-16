333a676a7f70feac7038c2676fe0ff87c93eb333
EduardoDadalt <eduardodadalt1@gmail.com>
2021-01-16 18:18:54 -0300

    1.1

--- context/Auth.tsx
+++ context/Auth.tsx
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import { createContext, useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import { auth } from "../config/fire";
 interface AuthProps {
   user: firebase.User;
@@ -7,14 +8,30 @@ interface AuthProps {
 const AuthContext = createContext<AuthProps | any>({});
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(auth.currentUser);
+  const [user, setUser] = useState<firebase.User | null>(auth.currentUser);
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
     var Unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user);
+      if (loading) setLoading(false);
     });
     return Unsubscribe;
   }, []);
-  return <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>;
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {loading ? (
+        <div
+          style={{ width: "100vw", height: "100vh" }}
+          className="d-flex align-items-center justify-content-center"
+        >
+          <Spinner animation="border" />
+        </div>
+      ) : (
+        children
+      )}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from "../context/Auth";
+import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
